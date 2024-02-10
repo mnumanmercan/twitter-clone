6cffe3c8d8253fd6b1cc4c2d574fd258bf8dba3c
Numan <numan.mercan24@gmail.com>
2024-02-10 17:57:13 +0300

    chore(button): created responsive button component

--- src/pages/layouts/main/sidebar/menu/index.jsx
+++ src/pages/layouts/main/sidebar/menu/index.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import classNames from "classnames";
 import { mainMenu } from "~/utils/consts";
+import Button from "~/components/button";
 
 const Menu = () => {
   return (
@@ -29,6 +30,25 @@ const Menu = () => {
           )}
         </NavLink>
       ))}
+      <button className="block group">
+          <div className="p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]">
+            <div className="w-[26.25px] h-[26.25px] relative">
+              <svg
+                viewBox="0 0 24 24"
+                width={26.25}
+                height={26.25}
+                className="block"
+              >
+                <path
+                  fill="#e7e9ea"
+                  d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z"
+                />
+              </svg>
+            </div>
+            <div className="pr-4 text-xl">More</div>
+          </div>
+      </button>
+      <Button size="large">Post</Button>
     </nav>
   );
 };
